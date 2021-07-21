5ef49688048ac2513f4b97d21747f0a443c09b49
Danilo Lacerda <danilo.lacerda@totvs.com.br>
2021-07-20 22:34:36 -0300

    doing class 3

--- src/components/Category/styles.ts
+++ src/components/Category/styles.ts
@@ -18,11 +18,11 @@ export const styles = StyleSheet.create({
     justifyContent: "space-between",
     paddingVertical: 20,
   },
-
   title: {
     fontFamily: theme.fonts.title700,
     color: theme.colors.heading,
     fontSize: 15,
+    marginTop: 15
   },
   check: {
     position: "absolute",
--- src/screens/AppointmentCreate/index.tsx
+++ src/screens/AppointmentCreate/index.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { RectButton } from "react-native-gesture-handler";
 import { Feather } from "@expo/vector-icons";
-import { Text, View } from "react-native";
+import { Text, View, ScrollView, KeyboardAvoidingView, Platform } from "react-native";
 
 import { theme } from "../../global/styles/theme";
 import { styles } from "./styles";
@@ -10,19 +10,31 @@ import { Background } from "../../components/Background";
 import { CategorySelect } from "../../components/CategorySelect";
 import { Header } from "../../components/Header";
 import { GuildIcon } from "../../components/GuildIcon";
+import { SmallInput } from "../../components/SmallInput";
+import { TextArea } from "../../components/TextArea";
+import { Button } from "../../components/Button";
 
 export function AppointmentCreate() {
   const [category, setCategory] = useState("");
 
   return (
+  <KeyboardAvoidingView
+  behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+  style={styles.container}
+  >
+    <ScrollView>
+
     <Background>
       <Header title="Agendar Partida" />
 
-      <Text style={[
-        styles.label,
-        {marginLeft: 24, marginTop: 36, marginBottom: 18}]}
-        >
-          Categoria</Text>
+      <Text
+        style={[
+          styles.label,
+          { marginLeft: 24, marginTop: 36, marginBottom: 18 },
+        ]}
+      >
+        Categoria
+      </Text>
 
       <CategorySelect
         hasCheckBox
@@ -47,7 +59,41 @@ export function AppointmentCreate() {
             />
           </View>
         </RectButton>
+
+        <View style={styles.field}>
+          <View>
+            <Text style={styles.label}>Dia e mês</Text>
+            <View style={styles.column}>
+              <SmallInput maxLength={2} />
+              <Text style={styles.divider}>/</Text>
+              <SmallInput maxLength={2} />
+            </View>
+          </View>
+          <View>
+            <Text style={styles.label}>Hora e minuto</Text>
+            <View style={styles.column}>
+              <SmallInput maxLength={2} />
+              <Text style={styles.divider}>:</Text>
+              <SmallInput maxLength={2} />
+            </View>
+          </View>
+        </View>
+        <View style={[styles.field, { marginBottom: 12 }]}>
+          <Text style={styles.label}>Descrição</Text>
+          <Text style={styles.caracteresLimit}>Max 100 caracteres</Text>
+        </View>
+        <TextArea
+          multiline
+          maxLength={100}
+          numberOfLines={5}
+          autoCorrect={false}
+        />
+        <View style={styles.footer}>
+          <Button title="Agendar"/>
+        </View>
       </View>
     </Background>
+    </ScrollView>
+    </KeyboardAvoidingView>
   );
 }
