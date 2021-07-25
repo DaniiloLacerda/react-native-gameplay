8563b038bcc9af546529f650608d6bac59bc493b
Danilo Lacerda <danio.lacerda@totvs.com.br>
2021-07-25 10:14:26 -0300

    finish class 3

--- src/screens/Guilds/styles.ts
+++ src/screens/Guilds/styles.ts
@@ -3,6 +3,11 @@ import { getStatusBarHeight } from "react-native-iphone-x-helper";
 
 export const styles = StyleSheet.create({
   container: {
-    
+    flex: 1,
+    alignItems: "center",
+    paddingTop: 24,
+  },
+  guilds: {
+    width: "100%",
   },
 });
